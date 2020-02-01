1a6bc4138d85ba59ddee1c39c1d7a3ca9105e01d
Marc Poppy <poppy.marc@gmail.com>
2020-02-01 22:19:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,11 @@ class Application
     #if before Time.now < noon "Good Morning"
     #else resp.write "Good Afternoon"
     resp = Rack::Response.new
-    if Time.now <
+    if Time.now < 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
     end
   end
-  
+
 end
